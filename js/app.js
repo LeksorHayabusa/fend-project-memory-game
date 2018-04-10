4e969fb1f3d5f2211531d0229c64dcda39f843fa
Alex Nikonov <31586394+LeksorHayabusa@users.noreply.github.com>
2018-04-10 16:50:52 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -17,29 +17,31 @@ $(function(){
 		gameOver = $('.game-over'),
 		deck = $('.deck');
 	let icons = [],
-		gameStats = ['1', 'hello', 'word', 'this'],
+		gameStats = [],
 		matchCounter, 
 		startTime,
 		failNumber,
-		movesCounter,
-		timer;
+		timer,
+		gameReady,
+		i = 0;//gameStats array index  and counter of plays numbers
+
 	cards.on('click.firstStart', restartGame);
 	restartButton.on('click', restartGame);
 
 	function restartGame(){
 		cards.off('click.firstStart');
+		clearInterval(timer);
+		gameStats.push([0,0,0]);//pushes game statistics array for current game
 		failNumber = 0;
-		movesCounter = 0;
 		closeModalWindow();
-		cards.removeClass('opened shown matched');
-		//TODO ADD RAMDOM CARD OPENING AND CLOSING
 		cards.off('click.openFirst');
 		cards.off('click.openSecond');
-		stopWatch(0);//starts stopWatch
 		moves.text(0);//it's not a function just text replacement
 		getCardsShuffled();	
-		setTimeout(openAllCards,1000);
-		findMatchingCards();
+		previewAllCards();
+		setTimeout(findMatchingCards, 3000); // that's workaround to wait until all cards are reviewed and closed
+		clearInterval(timer);
+		stopWatch(0);//starts stopWatch
 	}
 
 	function findMatchingCards(){
@@ -52,16 +54,20 @@ $(function(){
 		} else {
 			//opens last pair of cards
 			$('.card:not(.matched)').addClass('opened shown matched');
-			clearInterval(timer);
-			setTimeout(showModalWindow(),3000);
+			gameStats[i][0] = stars.children('li').length;
+			gameStats[i][2] = stopWatchDisplay.text();
+			setTimeout(showModalWindow,1000);
 		}
 
 		function openFirstCard(e){
 			cards.off('click.openFirst');
-			firstCardIndex = $(this).index();
-			cards.eq(firstCardIndex).addClass("opened shown");
-			firstCardClass = cards.eq(firstCardIndex).children('i').attr('class');
-			cards.on('click.openSecond', openSecondCard)
+			//check a click on already matched card
+			if (!$(this).hasClass('matched')){
+				firstCardIndex = $(this).index();
+				cards.eq(firstCardIndex).addClass("opened shown");
+				firstCardClass = cards.eq(firstCardIndex).children('i').attr('class');
+				cards.on('click.openSecond', openSecondCard)
+			} else {cards.on('click.openFirst', openFirstCard)}
 		}
 
 		function openSecondCard(e){
@@ -69,17 +75,20 @@ $(function(){
 			secondCardIndex = $(this).index();
 			cards.eq(secondCardIndex).addClass("opened shown");
 			secondCardClass = cards.eq(secondCardIndex).children('i').attr('class');
-			compareTwoCards();	
+			setTimeout(compareTwoCards, 500);	
 		}
 
 		function compareTwoCards(){
-			moves.text(++movesCounter);
-			if(secondCardIndex != firstCardIndex){ //is one card clicked twice?
+			let secondNotMatched = cards.eq(secondCardIndex).children('i');
+			moves.text(++gameStats[i][1]);
+			if(secondCardIndex != firstCardIndex && !cards.eq(secondCardIndex).hasClass('matched')){ //is one card clicked twice?
 				if(secondCardClass == firstCardClass){ //does one card match to second?
 					applyCardsMatching();
 				} else{
 					//this condition removes stars according to proficiency level
 					// 2 - means one star removed if 3 times fail
+					cards.eq(firstCardIndex).addClass("mismatched");
+					cards.eq(secondCardIndex).addClass("mismatched");
 					if(failNumber >= 2){
 						stars.children('li').last().remove();
 						failNumber = 0;
@@ -87,13 +96,13 @@ $(function(){
 					//this line is needed to create closure and store clicked elements' index
 					setTimeout(closeUnmatchedCards(firstCardIndex,secondCardIndex), 1000);
 				}
-			}
+			} else {(cards.on('click.openSecond', openSecondCard))}
 		}
 
 		function closeUnmatchedCards(index1,index2){
 			return function(){
-				cards.eq(index1).removeClass("opened shown");
-				cards.eq(index2).removeClass("opened shown");
+				cards.eq(index1).removeClass("opened shown mismatched");
+				cards.eq(index2).removeClass("opened shown mismatched");
 				return findMatchingCards();
 			}
 		}
@@ -101,14 +110,13 @@ $(function(){
 		function applyCardsMatching(){
 			failNumber--;
 			matchCounter--;
-			cards.eq(firstCardIndex).children('i').addClass('matched');
-			cards.eq(secondCardIndex).children('i').addClass('matched');
+			cards.eq(firstCardIndex).addClass('matched');
+			cards.eq(secondCardIndex).addClass('matched');
 			return findMatchingCards();
 		}
 	}
 
 	function getCardsShuffled(){
-		//TODO: MAKE THE ICONS ARRAY DOUBLE WITH DEEP COPY
 		icons = ['fa-diamond',
 				'fa-paper-plane-o',
 				'fa-anchor',
@@ -143,8 +151,7 @@ $(function(){
 		let minutes = 0,
 			seconds = 0,
 			secondString;
-		return timer = setInterval(function(){
-			console.log('the very stopWatch')
+		return timer = setInterval(()=>{
 			if (seconds+1 === 60 ){
 				minutes++;
 				seconds = 0;
@@ -158,32 +165,38 @@ $(function(){
 		}, 1000)
 	}
 
+	function showGameResult(){	
+			$('.stats-table  tbody').append('<tr><td>' + (i+1) + '</td><td>' +
+				gameStats[i][0] + '</td><td>' + gameStats[i][1] + '</td><td>' + gameStats[i][2] + '</td></tr>');
+			i++;
+	}
+
 	function showModalWindow(){
-		//TODO: develop modal window
-		gameOver.addClass('get-visible');
+		cards.addClass('cardsDisappearance');
+		setTimeout(()=>{
+			cards.addClass('get-invisible');
+		cards.removeClass('get-visible')
 		gameOver.removeClass('get-invisible');
-		cards.removeClass('get-visible opened');
-		cards.addClass('get-invisible');
-		$('.stats-table tbody').append('<tr>\n <td>' + gameStats[0] + '</td> \n <td>' +
-			gameStats[1] + '</td> \n' + '<td>' + gameStats[2] + '</td> \n' + 
-			'<td>' + gameStats[3] + '</td> \n </tr>');
+		gameOver.addClass('get-visible');
+		},1500);
+		showGameResult();
 	}
 
 	function closeModalWindow(){
-		//TODO: develop modal window
-		cards.addClass('get-visible');
-		cards.removeClass('get-invisible');
-		gameOver.removeClass('get-visible');
-		gameOver.addClass('get-invisible');
-		
+		cards.removeClass('get-invisible opened shown matched cardsDisappearance');
+		gameOver.removeClass('get-visible').addClass('get-invisible');
 	}
 
-	function openAllCards(){
-			cards.addClass("opened shown");
-			setTimeout(closeAllCards, 2000);
+	function previewAllCards(){
+		cards.each(function(i){
+			let delay = Math.floor(Math.random()*1000);
+			//starts show cards randomly
+			gameReady = setTimeout(()=>{cards.eq(i).addClass("opened shown")
+			//starts close cards randomly
+			setTimeout(()=>{cards.eq(i).removeClass("opened shown")}, 2000 + delay);
+			}, delay);
+		})
 	}
 
-	function closeAllCards(){
-		cards.removeClass("opened shown");
-	}
 }) //onload brackets
+
